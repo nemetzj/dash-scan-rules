18112068e949400159e37b20a2ed6a9d51a02141
Jacob <jakenemetz55@gmail.com>
2023-01-04 21:53:33 -0500

    Create combined artifact creation, update scan script

--- build-tools/artifact_creation.py
+++ build-tools/artifact_creation.py
@@ -48,6 +48,63 @@ class artifact_set:
         self.rules_dir = ARTIFACT_RULES_DIR
         self.meta_dir = ARTIFACT_META_DIR
         
+class custodian_rule:
+    def __init__(self, filename):
+        #TODO Validate rules schema for ALL files
+        fileInfo = self.get_one(filename)
+
+        self.root = fileInfo["root"] # ../scan-rules/custodian/aws/ec2/negative/ebs
+        self.path = fileInfo["path"] # ../scan-rules/custodian/aws/ec2/negative/ebs/ec2-ebs-unoptimized.json
+        self.file = fileInfo["file"] # ec2-ebs-unoptimized.json
+        self.name = fileInfo["name"] # ec2-ebs-unoptimized
+        self.json = self.get_json()
+
+
+    # Get single metadata json file
+    def get_one(self, filename):
+        for root, dirs, files in os.walk(CUSTODIAN_SCAN_RULES_DIR, topdown=True):
+           for file in files:
+                if file.endswith(filename):
+                    
+                    path = os.path.join(root, file)
+                    name = file.replace(".yml", "")
+                    
+                    output = {
+                        "root": root,
+                        "path": path,
+                        "file": file,
+                        "name": name
+                    }
+                    return output
+    
+
+    def get_json(self):
+        # Open YML file
+        with open(self.path, 'r') as yml_data:
+            configuration = yaml.safe_load(yml_data)
+
+        # Generate JSON file location
+        json_file_name = self.name + '.json'
+
+        with open(TEMP_DIR + json_file_name, 'w') as json_data:
+            json.dump(configuration, json_data, indent=2) # Write as temporary JSON file
+       
+        f = open(TEMP_DIR + json_file_name)
+        json_data = json.load(f) # Save JSON to variable
+
+        os.remove(TEMP_DIR + json_file_name) # Remove temporary JSON file
+
+        return json_data
+
+
+    ## Remove the outer scan rule element not needed for JSON
+    def trim_policy_json(self):
+        trimmed_json = self.json['policies'][0]
+        return trimmed_json
+
+
+
+
 
 
 class custodian_meta:
@@ -127,6 +184,16 @@ def get_all_meta_filenames(directory):
     return files_list
 
 
+#Get all scan-rule .yml files for a given directory and sub-directories    
+def get_all_scan_rule_filenames(directory):
+    files_list = []
+    for root, dirs, files in os.walk(directory, topdown=True):
+       for file in files:
+            if file.endswith(".yml"):
+                files_list.append(file)
+    return files_list
+
+
 #Write JSON file basaed on JSON input and output directory, file-name
 def write_json_file(json_input, output_dir, output_file):
     with open(output_dir + output_file, 'w') as output_file:
@@ -138,12 +205,76 @@ def write_json_file(json_input, output_dir, output_file):
 ## =================================================================================================
 
 
+def build_all_custodian_artifacts():
+
+    artifacts = artifact_set()
+    artifacts.create_directories()
+
+    ## =====================================================================
+    # Create Rules Artifacts
+    ## =====================================================================
+    rule_combined_json = []
+
+    file_list = get_all_scan_rule_filenames(CUSTODIAN_SCAN_RULES_DIR)
+    for file in file_list:
+        #print("file:", file)
+        rule = custodian_rule(file)
+        rule.json = rule.trim_policy_json()
+        ruleArtifact = rule.json
+
+
+        rule_combined_json.append(ruleArtifact) # Add to combined JSON
+        write_json_file(ruleArtifact, artifacts.rules_dir , rule.file)
+
+    #Write meta combined json
+    write_json_file(rule_combined_json, artifacts.artifact_dir , 'scan_rules.json')
+
+
+
+    ## =====================================================================
+    # Create Meta Artifacts
+    ## =====================================================================
+    meta_combined_json = []
+
+    file_list = get_all_meta_filenames(CUSTODIAN_SCAN_RULES_DIR)
+    for file in file_list:
+        #print("file:", file)
+        meta = custodian_meta(file)
+        metaArtifact = meta.createMetaFile()
+        meta_combined_json.append(metaArtifact) # Add to combined JSON
+        #print("metaArtifact:", metaArtifact)
+        write_json_file(metaArtifact, artifacts.meta_dir , meta.file)
+
+    #Write meta combined json
+    write_json_file(meta_combined_json, artifacts.artifact_dir , 'scan_meta.json') 
+
+
+
+def build_all_custodian_rules():
+
+    artifacts = artifact_set()
+    artifacts.create_directories()
+
+    rule_combined_json = []
+
+    file_list = get_all_scan_rule_filenames(CUSTODIAN_SCAN_RULES_DIR)
+    for file in file_list:
+        #print("file:", file)
+        rule = custodian_rule(file)
+        rule.json = rule.trim_policy_json()
+        ruleArtifact = rule.json
+
+
+        rule_combined_json.append(ruleArtifact) # Add to combined JSON
+        write_json_file(ruleArtifact, artifacts.rules_dir , rule.file)
+
+    #Write meta combined json
+    write_json_file(rule_combined_json, artifacts.artifact_dir , 'scan_rules.json')
+
+
 def build_all_custodian_meta():
     artifacts = artifact_set()
     artifacts.create_directories()
-    #print("Artifact2: ", artifacts.artifact_dir)
-    #print("Artifact2: ", artifacts.meta_dir)
-    #print("Artifact2: ", artifacts.rules_dir)
 
     meta_combined_json = []
     
@@ -158,23 +289,15 @@ def build_all_custodian_meta():
         write_json_file(metaArtifact, artifacts.meta_dir , meta.file)
 
     #Write meta combined json
-    write_json_file(meta_combined_json, artifacts.artifact_dir , 'scan-meta.json')
-    
-
-
-"""
-def build_single_custodian_meta(meta_file):
-    meta = custodian_meta(meta_file)
-    metaArtifact = meta.createMetaFile()
-    write_json_file(metaArtifact, CUSTODIAN_ARTIFACTS_DIR , meta.file)
-
-
-    metaControlArtifact = meta.createMetaControl()
-    write_json_file(metaControlArtifact, CUSTODIAN_ARTIFACTS_DIR , "control.json")
-"""
+    write_json_file(meta_combined_json, artifacts.artifact_dir , 'scan_meta.json')  
 
 
 
-build_all_custodian_meta()
 
+
+
+#build_all_custodian_artifacts()
+
+
+#build_all_custodian_meta()
 #build_single_custodian_meta("ec2-ebs-unencrypted-volumes-meta.json")
